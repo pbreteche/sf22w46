b64fe0bd36d9f1d4656a9259c2ac5c21363f3faf
Pierre Bretéché <pbreteche@dawan.fr>
2022-10-20 15:39:02 +0200

    Intégration de Bootstrap

--- assets/app.js
+++ assets/app.js
@@ -6,6 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
+import 'bootstrap/dist/css/bootstrap.css'
 import './styles/app.css';
 
 // start the Stimulus application
