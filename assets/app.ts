fcaa929791d4403eb81df1e92f7a2ed3fab96569
Pierre Bretéché <pbreteche@dawan.fr>
2022-10-20 17:35:57 +0200

    using typescript

--- assets/app.ts
+++ assets/app.ts
@@ -6,8 +6,10 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
 import './styles/app.scss';
+
+import './ts/script';
 
 // start the Stimulus application
 import './bootstrap';
