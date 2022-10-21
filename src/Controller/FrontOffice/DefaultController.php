35b65b78ea64e69725ed060dd7f2b2e4d7f43304
Pierre Bretéché <pbreteche@dawan.fr>
2022-10-21 15:10:27 +0200

    ux chartjs

--- src/Controller/FrontOffice/DefaultController.php
+++ src/Controller/FrontOffice/DefaultController.php
@@ -9,6 +9,8 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 class DefaultController extends AbstractController
 {
@@ -39,6 +41,40 @@ class DefaultController extends AbstractController
         return $this->render('front_office/default/index.html.twig', [
             'message' => 'Bonjour <script>console.log(\'coucou\');</script>',
             'demo_form' => $form->createView(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/ux/chart")
+     */
+    public function chartjs(ChartBuilderInterface $chartBuilder): Response
+    {
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+
+        $chart->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'backgroundColor' => 'rgb(255, 99, 132)',
+            'borderColor' => 'rgb(255, 99, 132)',
+            'datasets' => [
+                [
+                    'label' => 'Cookies eaten 🍪',
+                    'data' => [2, 10, 5, 18, 20, 30, 45],
+                    'backgroundColor' => 'red',
+                    'borderColor' => 'darkred',
+                    'cubicInterpolationMode' => 'monotone',
+                ],
+                [
+                    'label' => 'Km walked 🏃‍♀️',
+                    'data' => [10, 15, 4, 3, 25, 41, 25],
+                    'backgroundColor' => 'blue',
+                    'borderColor' => 'darkblue'
+                ],
+            ],
+        ]);
+
+        return $this->render('front_office/default/chartjs.html.twig', [
+            'chart' => $chart,
         ]);
     }
 }
