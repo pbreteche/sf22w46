d4d3fca38267b88bb6fc1479a5c83bbd8a8ffdae
Pierre Bretéché <pbreteche@dawan.fr>
2022-10-21 10:33:04 +0200

    Ajout entry point spécifique au back office

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.ts')
+    .addStyleEntry('back-office', './assets/styles/back_office.scss')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
