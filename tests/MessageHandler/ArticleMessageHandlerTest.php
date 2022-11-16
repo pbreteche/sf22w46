a834993241f32faa0f701280f0112842821db982
Pierre Bretéché <pbreteche@dawan.fr>
2022-11-16 16:24:58 +0100

    trying to mock LoggerInterface service

--- tests/MessageHandler/ArticleMessageHandlerTest.php
+++ tests/MessageHandler/ArticleMessageHandlerTest.php
@@ -5,12 +5,21 @@ namespace App\Tests\MessageHandler;
 use App\Entity\Article;
 use App\Message\ArticleMessage;
 use App\MessageHandler\ArticleMessageHandler;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class ArticleMessageHandlerTest extends KernelTestCase
 {
     public function testInvoke()
     {
+//        $logger = $this->createMock(LoggerInterface::class);
+//        $logger->expects(self::once())
+//            ->method('info')
+//            ->with('Nouvel article : Test title')
+//        ;
+//
+//        static::getContainer()->set(LoggerInterface::class, $logger);
+
         $articleMessageHandler = static::getContainer()->get(ArticleMessageHandler::class);
         $articleMessage = new ArticleMessage((new Article())->setTitle('Test title'));
 
